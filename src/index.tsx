967d488baf3c96d78e560a1c2af6267b96fe676c
Dusan Jovanov <jovanovdusan1@gmail.com>
2022-09-24 14:26:22 +0200

    setup - cra - react-router - tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import { Distance } from "./pages/Distance";
+import { Search } from "./pages/Search";
+import reportWebVitals from "./reportWebVitals";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="" element={<Search />} />
+      <Route path="/distance" element={<Distance />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/Search.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Search = () => {
+  return <div className="text-2xl">Search</div>;
+};
